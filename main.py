b4c4d01920bc919e9b91e0c210398899e3c03996
askiiart <90910526+askiiart@users.noreply.github.com>
2022-09-19 09:50:25 -0500

    Remove extra import

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import time
-
 print('This is my notes')
 print('This includes markdown files, and lots of python programs')
 print('\nAnd remember, do this:\n')
 import this
 
+# "that" must be installed from PyPI, there's not really any reason to though.
+# It just says what not to do in Python.
 print('\nAnd not that:\n')
 import that
